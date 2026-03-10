e00358a8fbe6b592a3c8bb69e7c295e2c9d3d8f3
Devanshu Pal <devanshup416@gmail.com>
2026-03-10 17:03:25 +0530

    feat: connect hw-dev to configured supabase project

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+window.HWDEV_BACKEND = {
+  provider: 'supabase',
+  supabaseUrl: 'https://cgpqtlpzyeiuhwgumcuj.supabase.co',
+  supabaseAnonKey: 'sb_publishable_y4LrAs3qdd11VBKA882Hkg_oCTRbcOR',
+  adminUsername: 'admin',
+  adminPassword: 'hwdev123'
+};
